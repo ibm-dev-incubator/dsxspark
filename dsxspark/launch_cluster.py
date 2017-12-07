c066cd1a78713f211f4204e40e0fcc20b47436de
Matthew Treinish <mtreinish@kortar.org>
2017-12-07 11:12:03 -0500

    Make SLSparkCluster.deploy_cluser() work
    This commit fixes the deploy_cluser() function and everything it calls
    to produce the expected result, which is launching all the nodes and
    creating an inventory file for running ansible spark-cluster-install.
    From here the next step is automating second ansible call for the
    cluster.

--- dsxspark/launch_cluster.py
+++ dsxspark/launch_cluster.py
@@ -2,6 +2,8 @@ import copy
 import os
 import tempfile
 
+import SoftLayer as sl
+
 from dsxspark import runner
 
 PLAYBOOK_PATH = os.path.join(
@@ -36,6 +38,7 @@ class SLSparkCluster(object):
         """
         self.cluster_name = cluster_name
         self.server_count = server_count
+        self.datacenter = datacenter
         ssh_keys = ssh_keys or []
         self.extra_vars = {
             'disk_size': disk_size,
@@ -44,35 +47,45 @@ class SLSparkCluster(object):
             'ssh_keys': ','.join(ssh_keys),
             "cluster_name": self.cluster_name,
             "domain": domain_name,
-            'datacenter': datacenter,
+            'datacenter': self.datacenter,
         }
-        self.inventory_file = os.path.join(tempfile.gettempdir(),
-                                           'spark_inv.ini')
+        self.inventory_file = os.path.join(
+            tempfile.gettempdir(), 'spark_inv_%s.ini' % self.cluster_name)
 
-    def _launch_worker(self, worker_number):
+    def _get_ip_addr(self, hostname):
+        sl_client = sl.create_client_from_env()
+        vs = sl.VSManager(sl_client)
+        server = vs.list_instances(hostname=hostname)[0]
+        return server['primaryIpAddress']
+
+    def _launch_worker(self, worker_number, master=None):
         hostname = self.cluster_name + '%02d' % worker_number
         worker_vars = copy.deepcopy(self.extra_vars)
         worker_vars['hostname'] = hostname
         runner.run_playbook_subprocess(LAUNCH_PLAYBOOK, worker_vars)
+        ip_addr = self._get_ip_addr(hostname)
+        if master:
+            self._write_master_inventory(hostname, ip_addr)
+        else:
+            self._write_node_inventory(hostname, ip_addr, worker_number)
 
-    def _write_node_inventory(self, node_num, ip_addr):
-        hostname = self.cluster_name + '%02d' % node_num
+    def _write_node_inventory(self, hostname, ip_addr, node_num):
         with open(self.inventory_file, 'a') as inv_file:
             inv_file.write(
-                "%s ansible_host=%s ansible_host_id=%s" % (
+                "%s ansible_host=%s ansible_host_id=%s\n" % (
                     hostname, ip_addr, node_num))
 
     def _write_master_inventory(self, hostname, ip_addr):
+        self.master_ip = ip_addr
         with open(self.inventory_file, 'a') as inv_file:
             inv_file.write("""
 [master]
 %s ansible_host %s ansible_host_id=1
 
 [nodes]
-""")
+""" % (hostname, ip_addr))
 
     def deploy_cluster(self):
-
         with open(self.inventory_file, 'a') as inv_file:
             inv_file.write("""[all:vars]
 ansible_connection=ssh
@@ -84,11 +97,9 @@ install_temp_dir=/tmp/ansible-install
 install_dir=/opt
 python_version=2
 """)
-        self._launch_worker(1)
-        self._write_master_inventory()
-        for node in range(2, self.server_count):
+        self._launch_worker(1, master=True)
+        for node in range(2, self.server_count + 1):
             self._launch_worker(node)
-            self._write_node_inventory()
 
     def _delete_worker(self, worker_number):
         hostname = self.cluster_name + '%02d' % worker_number
@@ -97,5 +108,5 @@ python_version=2
         runner.run_playbook_subprocess(DESTROY_PLAYBOOK, worker_vars)
 
     def collapse_cluster(self):
-        for node in range(self.server_count):
+        for node in range(self.server_count + 1):
             self._delete_worker(node)
